117b31d08f7fbf11d9d477b1a40d9252feb87572
Vladislav1 <vlad-pugovkin2@yandex.ru>
2022-11-19 16:09:10 +0300

    Add Project_3

--- Project_3/Program.cs
+++ Project_3/Program.cs
@@ -5,15 +5,15 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int[,] InitMatrix()
+int[,] InitMatrix(int m,int n)
 {
     Random rnd = new Random();
-    int[,] mtrx = new int[rnd.Next(2,10),rnd.Next(2,10)];
+    int[,] mtrx = new int[m,n];
     for (int i = 0; i < mtrx.GetLength(0); i++)
     {
         for (int j = 0; j < mtrx.GetLength(1); j++)
         {
-            mtrx[i,j] = rnd.Next(-10,11); 
+            mtrx[i,j] = rnd.Next(0,11); 
         }
     }
     return mtrx;
@@ -30,3 +30,40 @@ void PrintMatrix(int[,] mtrx)
         Console.WriteLine();
     }
 }
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"Среднее арифметическое столбца {i+1} равно: {arr[i]}");
+        }
+    
+}
+
+double[] SredneeArifm(int[,] mtrx)
+{
+    double sum = 0;
+    double[] srednee = new double[mtrx.GetLength(1)];
+    
+    for (int i = 0; i < mtrx.GetLength(1); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(0); j++)
+        {
+            sum = sum + mtrx[j,i];
+           
+        }
+        srednee[i] = sum/mtrx.GetLength(0);
+        sum = 0;
+    }
+    
+    return srednee;
+}
+Console.WriteLine("Введите кол-во строк матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = InitMatrix(m,n);
+Console.WriteLine();
+PrintMatrix(matrix);
+double[] result = SredneeArifm(matrix);
+Console.WriteLine("============================");
+PrintArray(result);
